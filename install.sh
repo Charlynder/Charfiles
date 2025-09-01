efc81adbd2a69c4f903fc4b892476cecc44cdf1b
Charlyder <alvarez.christian46@outlook.com>
2025-09-01 12:56:56 -0500

    test: test gum integration

--- install.sh
+++ install.sh
@@ -1,5 +1,22 @@
+#!usr/bin/env bash
+
 # Charfiles installer script
-#
 # date created: 09.01.2025
 
-echo 'Charfiles Installer'
+# --- gum ---
+# ensure gum is installed (silent)
+install_gum() {
+    if ! command -v gum &> /dev/null; then
+        tmpdir=$(mktemp -d)
+        curl -sSL "https://github.com/charmbracelet/gum/releases/latest/download/gum_$(uname -s)_$(uname -m).tar.gz" \
+        | tar -xz -C "$tmpdir"
+        sudo mv "$tmpdir/gum" /usr/local/bin/gum >/dev/null 2>&1
+        rm -rf "$tmpdir"
+    fi
+}
+
+# install gum if missing
+install_gum
+
+# --- title ---
+echo "--- 🐦‍🔥 Charfiles installer ---"
